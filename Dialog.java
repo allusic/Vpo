7ea7267a4e2f0b7dff3c712462239a56af06d4de
allusic <145779350+allusic@users.noreply.github.com>
2023-09-25 20:03:07 +0000

    Dialog

---
+++ Dialog.java
@@ -0,0 +1,18 @@
+import java .util.Scanner;
+
+public class Dialog {
+    public static void main ( String{}args){
+Scanner scanner=new Scanner(System.in);
+Sring name;
+int age;
+
+System.out.println(x:"Hello");
+System.out.println(X:"What is your name?");
+name=scanner.nextline();
+System.out.println(X:"What old are you?");
+age=scanner.nextint();
+
+System.out.println("Good,"+ name +"!");
+System.out.println("I now"+ age + " year");
+    }
+}
